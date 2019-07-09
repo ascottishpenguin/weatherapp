ca0ee1fedcada49b93c6e0410b80e63c0721c1e0
ascottishpenguin <crazyrandomscottishpenguin@hotmail.com>
2019-07-09 22:13:22 +0100

    All third party libraries now working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import {WeatherComponent} from './weather/weather.component';
+import {HomeComponent} from './home/home.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'weather', component: WeatherComponent},
+  {path: '**', redirectTo: 'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { WeatherComponent } from './weather/weather.component';
+import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    WeatherComponent
+    WeatherComponent,
+    HomeComponent,
+
   ],
   imports: [
     BrowserModule,
