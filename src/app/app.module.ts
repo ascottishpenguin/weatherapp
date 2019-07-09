6276aa14acf90831191a59bf59b238cb9c1a29ed
ascottishpenguin <crazyrandomscottishpenguin@hotmail.com>
2019-07-09 19:42:02 +0100

    Basic layout working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { WeatherComponent } from './weather/weather.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    WeatherComponent
   ],
   imports: [
     BrowserModule,
